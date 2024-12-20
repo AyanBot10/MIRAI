489e6f7b5dd7f196def1405f458b2d1215de3550
Ayan <ayanboom830@gmail.com>
2024-12-20 19:33:57 +0600

    Update run.js

--- run.js
+++ run.js
@@ -2,7 +2,7 @@ const { execSync } = require('child_process');
 
 try {
   console.log('--- Forcing the bot to start on port 3000 ---');
-  const port = 3000; // Force the port to 3000, ignoring any other port
+  const port = null; // Force the port to null, ignoring any other port
   execSync(`PORT=${port} npm start`, { stdio: 'inherit' });
 
 } catch (error) {
