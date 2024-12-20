888df792a43462421371c69a681f9a59d19a8ec0
Ayan <ayanboom830@gmail.com>
2024-12-20 19:38:56 +0600

    Update run.js

--- run.js
+++ run.js
@@ -1,9 +1,9 @@
 const { execSync } = require('child_process');
 
 try {
-  console.log('--- Forcing the bot to start on port 3000 ---');
-  const port = null; // Force the port to null, ignoring any other port
-  execSync(`PORT=${port} npm start`, { stdio: 'inherit' });
+  console.log('--- Forcing the bot to start on port ---');
+   //ignoring any other port
+  execSync(`npm start`, { stdio: 'inherit' });
 
 } catch (error) {
   console.error('Error occurred during the build process:', error);
